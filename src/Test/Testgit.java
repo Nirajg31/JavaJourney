acd0fd61a56de663c1fd4b5c3900073592475caf
Niraj <nirajnrjg@gmail.com>
2024-06-23 17:05:39 +0530

    test

--- src/Test/Testgit.java
+++ src/Test/Testgit.java
@@ -3,6 +3,20 @@ package Test;
 public class Testgit
 {
     public static void main(String[] args) {
-        System.out.println("Git branch test ")
+        System.out.println("Git branch test " +
+                "git pull" +
+                "git branch new-feature" +
+                "git checkout new-feature" +
+                "git branch" +
+                "git push origin new-feature" +
+                "git add ." +
+                "git commit -m \"Added new feature XYZ\"" +
+                "git push origin new-feature" +
+                "git checkout master" +
+                "git pull origin master" +
+                "git merge new-feature" +
+                "git commit -m \"Merge new-feature into master\"" +
+                "git push origin master");
+
     }
 }
